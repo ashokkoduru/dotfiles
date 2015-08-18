7604d674bb2f4cab3e5b045741608a8432b0388b
Haseeb ur Rahman <me@haseeb.co>
2015-08-18 19:47:40 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
-ln -s .dotfiles/vim .vim
-ln -s .dotfiles/vimrc .vimrc
-ln -s .dotfiles/zshrc .zshrc
+ln -s ~/.dotfiles/vim ~/.vim
+ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/oh-my-zsh ~/.oh-my-zsh
 apt-get install cmake
 sudo apt-get install build-essential -y 
 sudo apt-get install python2.7-dev
